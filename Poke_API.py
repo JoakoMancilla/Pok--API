2a909f6e671cdd2d48b82dc2e338304332b5bbad
JoakoMancillaDEV <JoakoMancillaVr@gmail.com>
2025-01-18 18:38:07 -0300

    Agregacion de Bucle para continuar con consultas

--- Poke_API.py
+++ Poke_API.py
@@ -1,6 +1,7 @@
 #Ejercicio de Consumo de API REST CON POKE-API en PYTHON
 from os import system
 import requests
+import os
 
 import matplotlib.pyplot as plot
 import matplotlib.image as img
@@ -15,36 +16,51 @@ class Poke_API():
         pass
 
     def menuInicio(self):
-        system("cls")
-        print ("Bienvenido a la Poké-API")
-        poke = input("Ingresa el Nombre/ID del Pokémon que desas Buscar: ")
+        while True:
+            system("cls")
+            print ("Bienvenido a la Poké-API")
+            poke = input("Ingresa el Nombre/ID del Pokémon que desas Buscar: ")
+            try:
+                respuesta = requests.get(f"https://pokeapi.co/api/v2/pokemon/{poke}") #Ya que la BD de Pokémon's es extensa el ultimo valor se lopregubtamos al usuario para que sea el el que decida que dato de Pokémon ver
 
-        respuesta = requests.get(f"https://pokeapi.co/api/v2/pokemon/{poke}") #Ya que la BD de Pokémon's es extensa el ultimo valor se lopregubtamos al usuario para que sea el el que decida que dato de Pokémon ver
+                if respuesta.status_code == 200:
 
-        if respuesta.status_code == 200:
+                    #Convertimos la respuesta JSON en un diccionario
+                    datos = respuesta.json()
 
-            #Convertimos la respuesta JSON en un diccionario
-            datos = respuesta.json()
+                    sprite_url = datos['sprites']['front_default']
 
-            sprite_url = datos['sprites']['front_default']
+                    if sprite_url:
+                        #Usamos urllib y PIL para descargar y leer la imagen
+                        with urllib.request.urlopen(sprite_url) as reponse:
+                            imagen = Image.open(reponse)
 
-            if sprite_url:
-                #Usamos urllib y PIL para descargar y leer la imagen
-                with urllib.request.urlopen(sprite_url) as reponse:
-                    imagen = Image.open(reponse)
+                            #Le damos los colores Correctos a la Imagen
+                            imagen = imagen.convert("RGBA")
 
-                    #Le damos los colores Correctos a la Imagen
-                    imagen = imagen.convert("RGBA")
+                            #Convertimos la imagen en un arry de numpy
+                            imagen_array = numpy.array(imagen)
 
-                    #Convertimos la imagen en un arry de numpy
-                    imagen_array = numpy.array(imagen)
+                        #Mostramos la Imagen con Matplotlib
+                        plot.title(datos['name'])
+                        imgplot = plot.imshow(imagen_array)
+                        plot.axis('off') #Oculta los ejes
+                        plot.show()
+                    else:
+                        print("El Pokémon no tiene Imagen asociada.")
+                
+                elif poke == 0:
+                    system("cls")
+                    print("Adios...")
+                    os._exit(1)
 
-                #Mostramos la Imagen con Matplotlib
-                plot.title(datos['name'])
-                imgplot = plot.imshow(imagen_array)
-                plot.axis('off') #Oculta los ejes
-                plot.show()
-            else:
-                print("El Pokémon no tiene Imagen asociada.")
-        else:
-            print("No se han encontrado datos.")
+
+                else:
+                    system("cls")
+                    print("No se han encontrado datos.")
+                    system("pause")
+            
+            except Exception as e:
+                system("cls")
+                print(f"Error: {e}")
+                system("pause")
